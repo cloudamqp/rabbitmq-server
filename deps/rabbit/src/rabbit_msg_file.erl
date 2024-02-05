b312571e8fe3bd7b4055abf40f4df1d815bef100
Péter Gömöri <peter@84codes.com>
2024-02-05 20:29:08 +0100

    WIP Optimise rabbit_msg_file:scan when we don't need the msg bodies

--- deps/rabbit/src/rabbit_msg_file.erl
+++ deps/rabbit/src/rabbit_msg_file.erl
@@ -7,11 +7,13 @@
 
 -module(rabbit_msg_file).
 
--export([scan/4]).
+-export([scan/4,
+         scan_ids/3]).
+
+-export([test/1,
+         test_orig/1]).
 
 %%----------------------------------------------------------------------------
-
--include_lib("rabbit_common/include/rabbit_msg_store.hrl").
 
 -define(INTEGER_SIZE_BYTES,      8).
 -define(INTEGER_SIZE_BITS,       (8 * ?INTEGER_SIZE_BYTES)).
@@ -33,6 +35,115 @@
             A).
 
 %%----------------------------------------------------------------------------
+test(File) ->
+    {ok, Fd} = file:open(File, [read, binary]),
+    try
+        scan_ids(Fd, filelib:file_size(File), [])
+    after
+        file:close(Fd)
+    end.
+
+test_orig(File) ->
+    {ok, Fd} = file:open(File, [read, binary]),
+    try
+        scan(Fd, filelib:file_size(File),
+             fun({MsgId,  TotalSize, Offset, _Msg}, Acc) ->
+                     [{MsgId, TotalSize, Offset} | Acc]
+             end,
+             [])
+    after
+        file:close(Fd)
+    end.
+
+
+scan_ids(FileHdl, FileSize, Acc) ->
+    scan_ids(FileHdl, FileSize, <<>>, 0, 0, Acc).
+
+
+scan_ids(_FileHdl, FileSize, _Data, FileSize, ScanOffset, Acc) ->
+    {ok, Acc, ScanOffset};
+scan_ids(FileHdl, _FileSize, _Data, _ReadOffset, ScanOffset, Acc) ->
+    %%Read = lists:min([?SCAN_BLOCK_SIZE, (FileSize - ReadOffset)]),
+    case read_file(FileHdl) of
+        {ok, Data1} ->
+            scan_header(Data1, ScanOffset, Acc, FileHdl);
+        _KO ->
+            {ok, Acc, ScanOffset, _KO, ?FUNCTION_NAME}
+    end.
+
+scan_header(<<>>, Offset, Acc, _FileHdl) ->
+    {ok, Acc, Offset, eof};
+scan_header(<<0:?INTEGER_SIZE_BITS, _Rest/binary>>, Offset, Acc, _FileHdl) ->
+    {ok, Acc, Offset, zero}; %% Nothing to do other than stop.
+scan_header(<<Size:?INTEGER_SIZE_BITS, MsgIdNum:?MSG_ID_SIZE_BITS, Rest/binary>>, Offset, Acc, FileHdl) ->
+    <<MsgId:?MSG_ID_SIZE_BYTES/binary>> =
+                <<MsgIdNum:?MSG_ID_SIZE_BITS>>,
+    TotalSize = Size + ?FILE_PACKING_ADJUSTMENT,
+    MsgSize = Size - ?MSG_ID_SIZE_BYTES,
+    skip_body(Rest, MsgSize, {MsgId, TotalSize, Offset}, Acc, FileHdl);
+scan_header(Rest, Offset, Acc, FileHdl) ->
+    MissingSize = ?INTEGER_SIZE_BYTES + ?MSG_ID_SIZE_BYTES - byte_size(Rest),
+    case read_file(FileHdl) of
+        %%{ok, Data} when byte_size(Data) >= MissingSize ->
+        {ok, Data} when byte_size(Data) >= 0 ->
+            %% FIXME subbinary?
+            <<Header2:MissingSize, Rest2/binary>> = Data,
+            %% Missig size =< 8+16-1
+            <<Size:?INTEGER_SIZE_BITS, MsgIdNum:?MSG_ID_SIZE_BITS>> = <<Rest/binary, Header2/binary>>,
+            TotalSize = Size + ?FILE_PACKING_ADJUSTMENT,
+            MsgSize = Size - ?MSG_ID_SIZE_BYTES,
+            <<MsgId:?MSG_ID_SIZE_BYTES/binary>> =
+                <<MsgIdNum:?MSG_ID_SIZE_BITS>>,
+            skip_body(Rest2, MsgSize, {MsgId, TotalSize, Offset}, Acc, FileHdl);
+%%            scan_header(<<Rest/binary, Data/binary>>, Offset, Acc, FileHdl);
+        _KO ->
+            {ok, Acc, Offset, _KO, ?FUNCTION_NAME}
+    end.
+
+skip_body(Data, MsgSize, Entry, Acc, FileHdl) ->
+    case Data of
+        <<_:MsgSize/binary, Rest/binary>> ->
+            scan_write_marker(Rest, Entry, Acc, FileHdl);
+        _ ->
+            %% need more data
+            MissingSize = MsgSize - byte_size(Data),
+            case read_file(FileHdl) of
+                {ok, Data2} when byte_size(Data) > 0 ->
+                    skip_body(Data2, MissingSize, Entry, Acc, FileHdl);
+                _KO ->
+                    {_MsgId, _TotalSize, Offset} = Entry,
+                    {ok, Acc, Offset, _KO, ?FUNCTION_NAME}
+            end
+    end.
+
+scan_write_marker(<<WriteMarker:?WRITE_OK_SIZE_BITS, Rest/binary>>, Entry, Acc, FileHdl) ->
+    Acc2 =
+        case WriteMarker of
+            ?WRITE_OK_MARKER ->
+                %% full entry available
+                [Entry|Acc];
+            _ ->
+                Acc
+        end,
+    {_MsgId, TotalSize, Offset} = Entry,
+    scan_header(Rest, Offset + TotalSize, Acc2, FileHdl);
+scan_write_marker(<<>>, Entry, Acc, FileHdl) ->
+    %% 1 byte missing
+    case read_file(FileHdl) of
+        {ok, Data} when byte_size(Data) > 0 ->
+            scan_write_marker(Data, Entry, Acc, FileHdl);
+        _KO ->
+            {_MsgId, _TotalSize, Offset} = Entry,
+            {ok, Acc, Offset, _KO, ?FUNCTION_NAME}
+    end.
+
+read_file(FileHdl) ->
+    read_file(FileHdl, ?SCAN_BLOCK_SIZE).
+
+read_file(FileHdl, Size) ->
+    %%file_handle_cache:read(FileHdl, Size)
+    file:read(FileHdl, Size).
+
 
 -spec scan(io_device(), file_size(), message_accumulator(A), A) ->
           {'ok', A, position()}.
@@ -44,7 +155,7 @@ scan(_FileHdl, FileSize, _Data, FileSize, ScanOffset, _Fun, Acc) ->
     {ok, Acc, ScanOffset};
 scan(FileHdl, FileSize, Data, ReadOffset, ScanOffset, Fun, Acc) ->
     Read = lists:min([?SCAN_BLOCK_SIZE, (FileSize - ReadOffset)]),
-    case file_handle_cache:read(FileHdl, Read) of
+    case read_file(FileHdl, Read) of
         {ok, Data1} ->
             {Data2, Acc1, ScanOffset1} =
                 scanner(<<Data/binary, Data1/binary>>, ScanOffset, Fun, Acc),
