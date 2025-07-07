f6bb7680523cf173bddc5e34869188806fc68f4b
Lois Soto Lopez <lois@84codes.com>
2025-07-07 12:27:59 +0200

    Test if add_member is called on a different server

--- deps/rabbit/test/quorum_queue_SUITE.erl
+++ deps/rabbit/test/quorum_queue_SUITE.erl
@@ -2804,7 +2804,7 @@ add_member_wrong_type(Config) ->
                           [<<"/">>, SQ, Server, voter, 5000])).
 
 add_member_already_a_member(Config) ->
-    [Server | _] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
+    [Server, Server2, _] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
     Ch = rabbit_ct_client_helpers:open_channel(Config, Server),
     QQ = ?config(queue_name, Config),
     ?assertEqual({'queue.declare_ok', QQ, 0, 0},
@@ -2812,7 +2812,10 @@ add_member_already_a_member(Config) ->
     %% idempotent by design
     ?assertEqual(ok,
                  rpc:call(Server, rabbit_quorum_queue, add_member,
-                          [<<"/">>, QQ, Server, voter, 5000])).
+                          [<<"/">>, QQ, Server, voter, 5000])),
+    ?assertEqual(ok,
+                 rpc:call(Server, rabbit_quorum_queue, add_member,
+                          [<<"/">>, QQ, Server2, voter, 5000])).
 
 add_member_not_found(Config) ->
     [Server | _] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
@@ -4886,7 +4889,6 @@ replica_states(Config) ->
 restart_after_queue_reincarnation(Config) ->
     [S1, S2, S3] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
     Ch = rabbit_ct_client_helpers:open_channel(Config, S1),
-    AuxCh = rabbit_ct_client_helpers:open_channel(Config, S2),
     QName = <<"QQ">>,
 
     ?assertEqual({'queue.declare_ok', QName, 0, 0},
@@ -4905,10 +4907,10 @@ restart_after_queue_reincarnation(Config) ->
     rabbit_ct_broker_helpers:mark_as_being_drained(Config, S3),
     ?assertEqual(ok, rabbit_control_helper:command(stop_app, S3)),
 
-    %% Delete and re-declare queue (on a different node) with the same name.
+    %% Delete and re-declare queue with the same name.
     rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_amqqueue, delete, [Q,false,false,<<"dummy_user">>]),
     ?assertEqual({'queue.declare_ok', QName, 0, 0},
-                 declare(AuxCh, QName, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+                 declare(Ch, QName, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
 
     % Now S3 should have the old queue state, and S1 and S2 a new one.
     St1 = rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_quorum_queue, status, [VHost, QName]),
@@ -4968,7 +4970,6 @@ restart_after_queue_reincarnation(Config) ->
 no_messages_after_queue_reincarnation(Config) ->
     [S1, S2, S3] = rabbit_ct_broker_helpers:get_node_configs(Config, nodename),
     Ch = rabbit_ct_client_helpers:open_channel(Config, S1),
-    AuxCh = rabbit_ct_client_helpers:open_channel(Config, S2),
     QName = <<"QQ">>,
 
     ?assertEqual({'queue.declare_ok', QName, 0, 0},
@@ -5000,7 +5001,7 @@ no_messages_after_queue_reincarnation(Config) ->
     %% Delete and re-declare queue with the same name.
     rabbit_ct_broker_helpers:rpc(Config, 0, rabbit_amqqueue, delete, [Q,false,false,<<"dummy_user">>]),
     ?assertEqual({'queue.declare_ok', QName, 0, 0},
-                 declare(AuxCh, QName, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
+                 declare(Ch, QName, [{<<"x-queue-type">>, longstr, <<"quorum">>}])),
 
     %% Bumping term in online nodes
     rabbit_ct_broker_helpers:rpc(Config, 1, rabbit_quorum_queue, transfer_leadership, [Q, S2]),
